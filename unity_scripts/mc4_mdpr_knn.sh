a96559f54e18daf41144d3f4d7ea564cfd225beb
Amit Hattimare <ahattimare@umass.edu>
2022-03-31 21:35:28 -0400

    embed query.

--- unity_scripts/mc4_mdpr_knn.sh
+++ unity_scripts/mc4_mdpr_knn.sh
@@ -27,8 +27,8 @@ export HF_DATASETS_CACHE="/home/ahattimare_umass_edu/scratch"
 
 echo "Embed query sentences."
 input=data/10k_examples_retriever.txt
-output=data/titles_10k_mdpr_ques.pt
-python src/mdpr.py --input $input --output $output --batch_size=256 --cuda "True" --load_save "True" --model_type="question"
+output=data/titles_10k_mdpr_pass.pt
+python src/mdpr.py --input $input --output $output --batch_size=256 --cuda "True" --load_save "True" --model_type="passage"
 
 #echo "Perform KNN search."
 #input=data/titles.txt
