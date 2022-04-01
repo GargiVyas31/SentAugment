c6bd405b804bf0660a120a232e7f0ae1e188b014
Amit Hattimare <ahattimare@umass.edu>
2022-03-31 22:16:31 -0400

    knn

--- unity_scripts/mc4_mdpr_knn.sh
+++ unity_scripts/mc4_mdpr_knn.sh
@@ -20,10 +20,10 @@ export HF_DATASETS_CACHE="/home/ahattimare_umass_edu/scratch"
 #python src/generate_data.py --num_rows=100000 --output $file_name --language=de --split_by=sentence
 #python src/compress_text.py --input $file_name
 
-#echo "Embed bank sentences."
-#input=data/mc4_fr100k.txt
-#output=data/mc4_fr100k_mdpr_ques.pt
-#python src/mdpr.py --input $input --output $output --batch_size=256 --cuda "True" --load_save "True" --model_type="question"
+echo "Embed bank sentences."
+input=data/mc4_de100k.txt
+output=data/mc4_de100k_mdpr_ques.pt
+python src/mdpr.py --input $input --output $output --batch_size=256 --cuda "True" --load_save "True" --model_type="question"
 
 #echo "Embed query sentences."
 #input=data/10k_examples_retriever.txt
@@ -44,16 +44,16 @@ bank_arr=("data/mc4_de100k.txt" "data/mc4_de100k.txt" "data/mc4_de100k.txt" "dat
 emb_arr=("data/mc4_de100k_mdpr_pass.pt" "data/mc4_de100k_mdpr_ques.pt" "data/mc4_de100k_mdpr_pass.pt" "data/mc4_de100k_mdpr_ques.pt")
 output_arr=("data/titles_1k_pass_de100k_pass_mdpr.txt" "data/titles_1k_pass_de100k_ques_mdpr.txt" "data/titles_1k_ques_de100k_pass_mdpr.txt" "data/titles_1k_ques_de100k_ques_mdpr.txt")
 
-for index in ${!input_arr[*]}; do
-  input=${input_arr[$index]}
-  input_emb=${input_emb_arr[$index]}
-  bank=${bank_arr[$index]}
-  emb=${emb_arr[$index]}
-  output=${output_arr[$index]}
-
-  echo "Running for ${input} ${input_emb} ${bank} ${emb} ${output}"
-  python src/flat_retrieve.py --input $input --input_emb $input_emb --bank $bank --emb $emb --K $K --pretty_print True --output $output
-done
+#for index in ${!input_arr[*]}; do
+#  input=${input_arr[$index]}
+#  input_emb=${input_emb_arr[$index]}
+#  bank=${bank_arr[$index]}
+#  emb=${emb_arr[$index]}
+#  output=${output_arr[$index]}
+#
+#  echo "Running for ${input} ${input_emb} ${bank} ${emb} ${output}"
+#  python src/flat_retrieve.py --input $input --input_emb $input_emb --bank $bank --emb $emb --K $K --pretty_print True --output $output
+#done
 
 #input=data/titles.txt
 #input_emb=titles_1k_mdpr_pass.pt
