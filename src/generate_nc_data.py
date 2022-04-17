d60c4233cc9905938760270212f108ee85021c0b
Amit Hattimare <ahattimare@umass.edu>
2022-04-17 00:36:36 -0400

    generate nc body of size 10k with tqdm.

--- src/generate_nc_data.py
+++ src/generate_nc_data.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from datasets import load_dataset
+from tqdm import tqdm
 
 
 def download_nc():
     nc_data = load_dataset("xglue", "nc")
     data = pd.DataFrame(nc_data["train"])
-    sample_articles = data["news_body"].sample(n=1000, random_state=20)
+    sample_articles = data["news_body"].sample(n=10_000, random_state=20)
 
-    with open("data/nc_body_1k.txt", "w") as f:
-        for article in sample_articles:
+    with open("data/nc_body_10k.txt", "w") as f:
+        for article in tqdm(sample_articles, desc="Creating NC body file"):
             f.write(article)
             f.write("\n")
 
