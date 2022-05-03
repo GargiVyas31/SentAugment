fd9b411f3abd1c81a4ba272e99b181de4b8c694b
Amit Hattimare <Amit-GH@users.noreply.github.com>
2022-05-02 23:14:08 -0400

    Query id retrieval (#17)
    * Code for retrieval with query_id.
    
    * minor changes.
    
    Co-authored-by: Amit Hattimare <ahattimare@umass.edu>

--- src/combine_bank_retrieval.py
+++ src/combine_bank_retrieval.py
@@ -7,23 +7,23 @@ parser = argparse.ArgumentParser(description="faiss index creation.")
 
 
 def combine_files_test():
-    # The columns are [query_id, sent_id, sent_score, sent]
+    # The columns are [query_id, sent_id, sent_score, query_label, sent]
     data1 = [
-        [1, 1, 1.23, "sent1"],
-        [2, 4, 1.20, "sent4"],
-        [2, 5, 1.54, "sent5"],
-        [2, 3, 1.43, "sent3"],
-        [1, 2, 1.34, "sent2"],
-        [1, 3, 1.43, "sent3"],
+        [1, 1, 1.23, 0, "sent1"],
+        [2, 4, 1.20, 8, "sent4"],
+        [2, 5, 1.54, 8, "sent5"],
+        [2, 3, 1.43, 8, "sent3"],
+        [1, 2, 1.34, 0, "sent2"],
+        [1, 3, 1.43, 0, "sent3"],
     ]
 
     data2 = [
-        [1, 10, 1.28, "sent10"],
-        [2, 40, 1.20, "sent40"],
-        [3, 50, 1.54, "sent50"],
-        [2, 30, 1.57, "sent30"],
-        [1, 20, 1.34, "sent20"],
-        [1, 30, 1.55, "sent30"],
+        [1, 10, 1.28, 0, "sent10"],
+        [2, 40, 1.20, 8, "sent40"],
+        [3, 50, 1.54, 5, "sent50"],
+        [2, 30, 1.57, 8, "sent30"],
+        [1, 20, 1.34, 0, "sent20"],
+        [1, 30, 1.55, 0, "sent30"],
     ]
 
     df1 = pd.DataFrame(data1)
@@ -58,8 +58,8 @@ def combine_files(file_name_template: str, num_files: int, output_file: str, k: 
         # group by query_id, sort each by decreasing sent_score, then keep top k.
         df = df.groupby([0]).apply(lambda x: x.sort_values(by=[2], ascending=False).head(k))
 
-    # Save combined result in disk. Only save sent_id and sentence with no header.
-    df.to_csv(output_file, columns=[1, 3], header=False, index=False)
+    # Save combined result in disk. Only save [sent_id, query_label, sentence] with no header.
+    df.to_csv(output_file, columns=[1, 3, 4], header=False, index=False)
 
 
 if __name__ == '__main__':
@@ -69,9 +69,10 @@ if __name__ == '__main__':
     parser.add_argument("--K", type=int, required=True, help="number of examples to keep for each query sentence.")
     args = parser.parse_args()
 
-    # combine_files_test()
     t0 = time.time()
     combine_files(args.file_name_template, args.num_files, args.output_file, args.K)
     print(f"total time combining files: {(time.time() - t0):.3f}s.")
+
+    # combine_files_test()
 
     exit(0)
--- src/generate_nc_data.py
+++ src/generate_nc_data.py
@@ -1,12 +1,15 @@
+import csv
+
 import pandas as pd
 from datasets import load_dataset
 from tqdm import tqdm
+import numpy as np
 
 
 def download_nc():
     nc_data = load_dataset("xglue", "nc")
     data = pd.DataFrame(nc_data["train"])
-    data = data[["news_body", "news_category"]].sample(n=100_000, random_state=10)
+    data = data[["news_body", "news_category"]].sample(n=10_000, random_state=10)
 
     # Find the data distribution.
     data_perc = data["news_category"].value_counts().apply(lambda x: x*100/data.shape[0])
@@ -28,11 +31,32 @@ def download_nc():
     # shuffle the data.
     data_balanced.sample(frac=1).reset_index(drop=True)
 
-    with open("data/nc_body_100k.txt", "w") as f:
-        for article in tqdm(data_balanced["news_body"], desc="Creating NC body file"):
-            f.write(article)
-            f.write("\n")
+    output_file = "data/nc_body_rand_10k.csv"
+
+    with open(output_file, "w") as output_file:
+        csv_writer = csv.writer(output_file)
+        for article, category in tqdm(zip(data_balanced["news_body"], data_balanced["news_category"]),
+                            desc="Creating NC body file",
+                            total=data_balanced.shape[0]):
+            csv_writer.writerow([category, article])
+
+
+def check_data_length_distribution():
+    input_file = "../data/nc_body_rand_10k.csv"
+
+    df_input = pd.read_csv(input_file, header=None)
+
+    def word_count(sent: str) -> int:
+        return len(sent.split(' '))
+
+    word_counts = df_input[1].apply(word_count).to_numpy()
+
+    for perc in range(0, 101, 10):
+        print(f"{perc} percentile index: {np.percentile(word_counts, perc):.2f}")
+        
+    print("done")
 
 
 if __name__ == '__main__':
-    download_nc()
+    # download_nc()
+    check_data_length_distribution()
